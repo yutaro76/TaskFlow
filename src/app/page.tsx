0f64f0b9acf52180932c7691a0f44fcdc5083353
yutaro okuda <yutaro.okuda.76@gmail.com>
2025-01-20 09:59:05 +0800

    [Customizing components] ボタンなどのコンポーネントのデフォルトの値を変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,16 @@ import { Button } from '@/components/ui/button';
 export default function Home() {
   return (
     <>
-      <Button>Click me</Button>
-      <p className='text-red-500 font-semibold'>test123</p>
+      <input />
+      <Button variant='primary' size='sm'>
+        primary
+      </Button>
+      <Button variant='destructive'>destructive</Button>
+      <Button variant='outline'>outline</Button>
+      <Button variant='secondary'>secondary</Button>
+      <Button variant='ghost'>phost</Button>
+      <Button variant='muted'>muted</Button>
+      <Button variant='teritary'>teritary</Button>
     </>
   );
 }
