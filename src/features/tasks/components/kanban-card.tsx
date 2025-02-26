0c046043562dca13175f73e581da25c3e8486cb2
yutaro okuda <yutaro.okuda.76@gmail.com>
2025-02-26 09:38:43 +0800

    KanbanでタスクにマウスホバーでTooltipでタイトルをすべて表示

--- src/features/tasks/components/kanban-card.tsx
+++ src/features/tasks/components/kanban-card.tsx
@@ -5,17 +5,27 @@ import { DottedSeparator } from '@/components/dotted-separator';
 import { MemberAvatar } from '@/features/members/components/member-avatar';
 import { TaskDate } from './task-date';
 import { ProjectAvatar } from '@/features/projects/components/project-avatar';
+import { Tooltip } from 'react-tooltip';
 
 interface KanbanCardProps {
   task: Task;
 }
 
 export const KanbanCard = ({ task }: KanbanCardProps) => {
+  const taskNameLength = task.name.length > 27;
   return (
-    <div className='bg-white p-2.5 mb-1.5 rounded shadow-sm space-y-3'>
+    <div
+      className='bg-white p-2.5 mb-1.5 rounded shadow-sm space-y-3'
+      data-tooltip-id={taskNameLength ? 'kanban-task' : undefined}
+      data-tooltip-place='top'
+      data-tooltip-content={taskNameLength ? task.name : undefined}
+    >
+      {taskNameLength && <Tooltip id='kanban-task' />}
       <div className='flex items-start justify-between gap-x-2'>
         {/* テキストを1行に制限し、それ以上のテキストは省略記号（...）で表示されるようにする */}
-        <p className='text-sm line-clamp-1'>{task.name}</p>
+        <p className='text-sm line-clamp-1  overflow-hidden break-all'>
+          {task.name}
+        </p>
         <TaskActions id={task.$id} projectId={task.projectId}>
           {/* stroke-1: SVG要素のストローク幅を 1 に設定。shrink-0: Flexboxコンテナ内で要素が縮小しないように設定。 */}
           <MoreHorizontal className='size-[18px] stroke-1 shrink-0 text-neutral-700 hover:opacity-75 transition' />
